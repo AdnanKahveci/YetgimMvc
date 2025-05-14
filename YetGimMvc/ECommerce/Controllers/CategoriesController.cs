8b1faf908a5051a3ae04463363cf6225798028ee
AdnanKahveci <kahveciadnan258@gmail.com>
2025-05-14 12:57:35 +0300

    Attributes

--- YetGimMvc/ECommerce/Controllers/CategoriesController.cs
+++ YetGimMvc/ECommerce/Controllers/CategoriesController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerce.Models;
+using ECommerce.Services.Abstracts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.Controllers;
 
-public class CategoriesController : Controller
+public class CategoriesController(ICategoryService categoryService) : Controller
 {
-    // GET
     public IActionResult Index()
+    {
+        var categories = categoryService.GetAll();
+        return View(categories);
+    }
+
+
+    [HttpGet]
+    public IActionResult Add()
     {
         return View();
     }
+
+    [HttpPost]
+    public IActionResult Add(Category category)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+        
+        categoryService.Add(category);
+        return RedirectToAction("Index","Categories");
+    }
+
+    [HttpGet]
+    public IActionResult Detail(int id)
+    {
+        var category = categoryService.GetById(id);
+        return View(category);
+    }
+    
+    
 }
--- YetGimMvc/ECommerce/obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
+++ YetGimMvc/ECommerce/obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84b4ec913474fd142faa02aa3d39db6e13884293")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32d329029b7053283002ab604e0f780e965212f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
